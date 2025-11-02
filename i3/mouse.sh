ada97b3817b9a2b31108d829fcbed98afa1a9e97
KartoffelPhantom <kartuno@proton.me>
2025-11-02 03:15:51 +0100

    Update system configuration

--- i3/mouse.sh
+++ i3/mouse.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
-sleep 10
-id=$(xinput list --id-only "Compx SCYROX 8K Dongle")
-echo "$(date): device id is $id" >> ~/.config/i3/disable-middle-emulation.log
-if [ -n "$id" ]; then
-  xinput set-prop "$id" "libinput Middle Emulation Enabled" 0
-  echo "$(date): set middle emulation off for device $id" >> ~/.config/i3/disable-middle-emulation.log
-else
-  echo "$(date): device not found" >> ~/.config/i3/disable-middle-emulation.log
-fi
+max_tries=10
+count=0
+
+while [ $count -lt $max_tries ]; do
+  id=$(xinput list --id-only "Compx SCYROX 8K Dongle")
+  if [ -n "$id" ]; then
+    echo "$(date): device id is $id" >> ~/.config/i3/disable-middle-emulation.log
+    xinput set-prop "$id" "libinput Middle Emulation Enabled" 0
+    echo "$(date): set middle emulation off for device $id" >> ~/.config/i3/disable-middle-emulation.log
+    exit 0
+  else
+    echo "$(date): device not found, try $((count+1))" >> ~/.config/i3/disable-middle-emulation.log
+    sleep 4
+  fi
+  count=$((count+1))
+done
+
+echo "$(date): device never found. Giving up." >> ~/.config/i3/disable-middle-emulation.log
+exit 1
 
