4157237b3bdaf548deb758247c570ba04067e2e3
KartoffelPhantom <kartuno@proton.me>
2025-12-28 13:01:39 +0100

    Update system configuration

--- flake.nix
+++ flake.nix
@@ -3,12 +3,25 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
  };
-    outputs = { self, nixpkgs, ...}: {
-      nixosConfigurations.kys = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";          
+   
+   outputs = { self, nixpkgs, home-manager, ...}: 
+      let
+        lib = nixpkgs.lib;
+	system = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+      in {
+         nixosConfigurations.kys = nixpkgs.lib.nixosSystem {
+          inherit system;         
           modules = [ ./configuration.nix ];
     };
+  homeConfigurations = {
+          kartoma = home-manager.lib.homeManagerConfiguration {
+	  inherit pkgs;     
+          modules = [ ./home.nix ]; 
+     };
   };
+};
 }
-
